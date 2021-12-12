460f52c856468597e54ab22865fe6529c70810b2
unknown <mahesh970098@gmail.com>
2021-12-12 15:44:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,160 @@
+// Write Javascript Here
+const baseUrl = "http://localhost:3000/users";
+const headers = {
+    'Content-type': 'application/json; charset=UTF-8'
+};
+
+
+
+getUsersRequest().then(users =>{
+    //This function has been implemented already for you
+    const tableEl = document.getElementById("table");
+    for (const user of users) {
+        tableEl.appendChild(createTableRow(user))
+    }
+})
+
+
+
+
+
+
+
+
+function addNewUser(){
+    //TODO: Implement me
+}
+
+
+
+function editUser(id, userName){
+    //TODO: implement me
+}
+
+function deleteUser(id){
+    //TODO: implement me
+
+}
+
+
+
+
+//CRUD HELPER METHODS
+function createUserRequest(user){
+    return fetch(baseUrl, {
+        method: 'POST',
+        headers: headers,
+        body:JSON.stringify(user),
+    }).then(response => response.json())
+}
+
+
+function  getUsersRequest()  {
+    return fetch(baseUrl, {
+        method: 'GET',
+    }).then(response => response.json())
+}
+
+function  deleteUserRequest(id)  {
+    return fetch(`${baseUrl}/${id}`, {
+        method: 'DELETE',
+    }).then(response => response.json())
+}
+
+
+function updateUserRequest(user){
+    return fetch(`${baseUrl}/${user.id}`, {
+        method: 'PATCH',
+        headers: headers,
+        body:JSON.stringify(user),
+    }).then(response => response.json())
+}
+
+
+//HELPER METHODS
+function createTableRow(user){
+    var tr = document.createElement("tr");
+    tr.innerHTML = `<td>${user.name}</td> <td><a href="#" onclick="editUser(${user.id}, '${user.name}')">Edit</a> / <a href="#" onclick="deleteUser(${user.id})">Delete</a></td>`;
+    return tr;
+}
+
+
+var selectedRow = null
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["empCode"] = document.getElementById("empCode").value;
+    formData["salary"] = document.getElementById("salary").value;
+    formData["city"] = document.getElementById("city").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.empCode;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.salary;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.city;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("fullName").value = "";
+    document.getElementById("empCode").value = "";
+    document.getElementById("salary").value = "";
+    document.getElementById("city").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("empCode").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("city").value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.fullName;
+    selectedRow.cells[1].innerHTML = formData.empCode;
+    selectedRow.cells[2].innerHTML = formData.salary;
+    selectedRow.cells[3].innerHTML = formData.city;
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+function validate() {
+    isValid = true;
+    if (document.getElementById("fullName").value == "") {
+        isValid = false;
+        document.getElementById("fullNameValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
+            document.getElementById("fullNameValidationError").classList.add("hide");
+    }
+    return isValid;
+}
